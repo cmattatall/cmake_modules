fce76faf972418a17bab9bfd4467c9f6024dc54b
Carl Mattatall <cmattatall2@gmail.com>
2022-01-02 12:50:03 -0400

    [Add] additional cpack modules

---
+++ cmake/cpack/cpack_deb.cmake
@@ -0,0 +1,47 @@
+cmake_minimum_required(VERSION 3.21)
+
+#################################
+# Debian packages common configs
+#################################
+list(APPEND CPACK_GENERATOR "DEB")
+
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${CPACK_PACKAGE_VENDOR} <${CPACK_PACKAGE_CONTACT}>")
+set(CPACK_DEBIAN_PACKAGE_DESCRIPTION ${PROJECT_DESCRIPTION})
+set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+set(CPACK_DEBIAN_PACKAGE_GENERATE_SHLIBS ON) # generate dependencies for upstream packages
+set(CPACK_DEBIAN_PACKAGE_GENERATE_SHLIBS_POLICY ">=")
+set(CPACK_DEBIAN_PACKAGE_VERSION_MAJOR ${CPACK_PACKAGE_VERSION_MAJOR})
+set(CPACK_DEBIAN_PACKAGE_VERSION_MINOR ${CPACK_PACKAGE_VERSION_MINOR})
+set(CPACK_DEBIAN_PACKAGE_VERSION_PATCH ${CPACK_PACKAGE_VERSION_PATCH})
+set(CPACK_DEBIAN_PACKAGE_VERSION ${CPACK_DEBIAN_PACKAGE_VERSION_MAJOR}.${CPACK_DEBIAN_PACKAGE_VERSION_MINOR}.${CPACK_DEBIAN_PACKAGE_VERSION_PATCH})
+set(CPACK_DEBIAN_PACKAGE_FILE_BASE_NAME ${CPACK_PACKAGE_NAME}-${CPACK_DEBIAN_PACKAGE_VERSION})
+set(CPACK_DEBIAN_PACKAGE_CONTROL_STRICT_PERMISSION ON)
+                                    
+
+if(NOT CMAKE_CROSSCOMPILING)
+    # Configure the debian package architecture
+    find_program(DPKG_ARCH_EXECUTABLE dpkg-architecture REQUIRED)
+    execute_process(
+        COMMAND ${DPKG_ARCH_EXECUTABLE} -qDEB_HOST_ARCH
+        OUTPUT_VARIABLE CMAKE_DEB_HOST_ARCH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE ${CMAKE_DEB_HOST_ARCH})
+
+else()
+    message(FATAL_ERROR "TODO: Add CPACK_DEBIAN_FILE_NAME configuration for cross-compiled projects")
+endif(NOT CMAKE_CROSSCOMPILING)
+
+
+######################################
+# Debian package component configs
+######################################
+# careful about capitalization: https://cmake.org/cmake/help/v3.6/module/CPackDeb.html
+# CPACK_DEBIAN_<ALL_CAPS_COMPONENT>_FILE_NAME controls the name of the packaged debian output file
+
+#[[
+https://cmake.org/cmake/help/v3.6/module/CPackDeb.html
+CPACK_DEBIAN_FILE_NAME may be set to DEB-DEFAULT to allow CPackDeb to generate package file name by itself in deb format:
+<PackageName>_<VersionNumber>-<DebianRevisionNumber>_<DebianArchitecture>.deb
+#]]
+set(CPACK_DEBIAN_FILE_NAME "DEB-DEFAULT")
---
+++ cmake/cpack/cpack_postinst.cmake
@@ -0,0 +1,35 @@
+cmake_minimum_required(VERSION 3.21)
+
+######################################
+# Configure Post-install steps
+######################################
+# Write the post-install script
+set(PROJECT_CONFIG_FILE_STAGING_DIR "${PROJECT_BINARY_DIR}/configFiles") 
+
+if(NOT EXISTS ${PROJECT_BINARY_DIR}/configFiles)
+    file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/configFiles)
+endif(NOT EXISTS ${PROJECT_BINARY_DIR}/configFiles)
+
+file(
+    WRITE ${PROJECT_BINARY_DIR}/configFiles/postinst
+    "#!/bin/bash\necho \"performing postinst steps for ${PROJECT_NAME}\"\nldconfig\n"
+)
+set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA ${PROJECT_BINARY_DIR}/configFiles/postinst)
+
+
+# Write, set permissions, and install the ld config file
+set(PROJECT_LDCONFIG_FILE ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.conf)
+file(
+    WRITE ${PROJECT_LDCONFIG_FILE}
+    "# ${PROJECT_NAME} default configuration\n${CPACK_PACKAGING_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}\n"
+)
+
+install(
+    FILES ${PROJECT_LDCONFIG_FILE}
+    DESTINATION "/etc/ld.so.conf.d/${PROJECT_NAME}/"
+    PERMISSIONS
+        OWNER_WRITE OWNER_READ
+        GROUP_READ
+        WORLD_READ
+    COMPONENT lib
+)
