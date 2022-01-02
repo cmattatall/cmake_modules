b693da8c62b24a1a95302369b642abad11ebfc5e
Carl Mattatall <cmattatall2@gmail.com>
2022-01-02 18:50:52 -0400

    [Add] tests teardown (package_api example project uninstall) script

--- scripts/package_api.uninstall.sh
+++ scripts/package_api.uninstall.sh
@@ -6,14 +6,18 @@ WORKDIR=$(pwd)
 
 if [ ! -f "$(pwd)/.gitignore" ]; then
     echo "Script not being invoked from repository root! Exiting ... "
-    exit -1
+    exit -1 
 fi
 
 function uninstall () {
-
-
-
-
+    # this is hardcoded between the install and uninstall
+    # scripts but that's alright for now
+    local BUILD_DIR="${WORKDIR}/build" 
+    while IFS= read -r pkg; do
+        if [ $pkg != "" ]; then
+            sudo dpkg -r $pkg
+        fi
+    done < "${BUILD_DIR}/package-list.txt"
 }
 
 function main () {
