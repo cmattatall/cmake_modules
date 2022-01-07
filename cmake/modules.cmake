4bfae5455c55aa42abda96422e74919e3f0b5d03
Carl Mattatall <cmattatall2@gmail.com>
2022-01-07 12:52:43 -0400

    [Remove] unused cmake module header

--- cmake/modules.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.21)
-
-include(${CMAKE_CURRENT_LIST_DIR}/utils/utils_api)
-include(${CMAKE_CURRENT_LIST_DIR}/packaging/package_api)
-include(${CMAKE_CURRENT_LIST_DIR}/analysis/analysis_api)
-include(${CMAKE_CURRENT_LIST_DIR}/codegen/git/git-metadata)
-include(${CMAKE_CURRENT_LIST_DIR}/profiling/profiling_api)
-include(${CMAKE_CURRENT_LIST_DIR}/testing/testing_api)
-include(${CMAKE_CURRENT_LIST_DIR}/docugen/docugen_api)
