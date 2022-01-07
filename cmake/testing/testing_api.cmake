816db6426f1aac3ab56ec25e169776a5c354264d
Carl Mattatall <cmattatall2@gmail.com>
2022-01-07 13:19:31 -0400

    [Fix] bug in path for include modules of testing_api

--- cmake/testing/testing_api.cmake
+++ cmake/testing/testing_api.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.21)
 
-include(${CMAKE_CURRENT_LIST_DIR}/gtest/gtest_setup)
+include(${CMAKE_CURRENT_LIST_DIR}/gtest/gtest_setup.cmake)
 
 
 function(testing_discover_tests test_runner_target)
