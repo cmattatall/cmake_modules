a322503caad9c6c3a0b5ae20d4e828ef97dbe342
Carl Mattatall <cmattatall2@gmail.com>
2022-01-15 13:02:07 -0400

    [Remove] package_create_libraries from library api
    It was not aligned with the spirit of the API. There were also
    silent side affects regarding intermediate targets in the package
    list that could introduce future data-races for shared build roots
    in the future.

--- src/baz.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <json/json.h>
-
-int foo() {
-  Json::Value myJsonVal;
-  myJsonVal["baz"] = "bazval";
-  std::cout << myJsonVal.toStyledString() << std::endl;
-  return 0;
-}
