40920e11429a5612388591fc9fe3b93eb16fa5dc
Carl Mattatall <cmattatall2@gmail.com>
2022-01-16 19:26:46 -0400

    [Update] project scope. Per-package installs are no longer performed

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -4,22 +4,32 @@ set -e
 
 
 function main () {
-    cmake -S . \
-        -B build \
-        --log-level=debug
+    cmake -S . -B build
+    cmake --build build
+
+
+    # LOCAL_CMAKE_MODULE_PATH=""
+    # for cmake_module_configfile in $(find cmake -name "*Config\.cmake"); do
+    #     cmake_module_dir=$(dirname ${cmake_module_configfile})
+    #     LOCAL_CMAKE_MODULE_PATH="${LOCAL_CMAKE_MODULE_PATH};${cmake_module_dir};"
+    # done
+    # echo "LOCAL_CMAKE_MODULE_PATH:${LOCAL_CMAKE_MODULE_PATH}"
+
+    # for cmakelists in $(find tests -name "*CMakeLists\.txt"); do
+    #     set +e
+    #     source_dir=$(dirname ${cmakelists})
+    #     cmake -S ${source_dir} -B ${source_dir}/build -DCMAKE_PREFIX_PATH="${LOCAL_CMAKE_MODULE_PATH}"
+    #     cmake --build ${source_dir}/build
+    #     pushd ${source_dir}/build
+    #         cpack
+    #     popd
+    #     rm -r ${source_dir}/build
+    #     set -e
+    # done
+
     pushd build
-        make -j$(nproc)
-        ctest \
-        --extra-verbose \
-        --output-on-failure \
-        --output-log test.log \
-        -R PackagerApi_tests_positive_1_configure
-
-        #-R Pack* # for now, we will just be testing the package api
-
-
-        #cpack
-        #find . -name "*\.deb" -exec dpkg -i {} \;
+        cpack
+        #find packages/ -name "*\.deb" -exec dpkg -i {} \;
     popd
 }
 
