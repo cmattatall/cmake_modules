3cd8f2fcfe5f49a4f7e6ba7d219166a00f9e1663
Carl Mattatall <cmattatall2@gmail.com>
2022-01-27 09:50:51 -0400

    [Add] work for graphviz cleanup

--- cmake/docugen/GraphvizDocugen/GraphvizDocugenConfig.cmake
+++ cmake/docugen/GraphvizDocugen/GraphvizDocugenConfig.cmake
@@ -5,22 +5,36 @@ cmake_minimum_required(VERSION 3.10)
 
 function(GraphvizDocugen_make_target_graph)
     if(${CMAKE_VERSION} VERSION_GREATER "3.21.0")
+        set(GRAPHVIZ_WORKDIR ${CMAKE_BINARY_DIR})
         set(DOT_EXECUTABLE dot)
         find_program(DOT_EXE_PATH "${DOT_EXECUTABLE}${CMAKE_EXECUTABLE_SUFFIX}")
-        if(${DOT_EXE_PATH} STREQUAL DOT_EXE_PATH-NOTFOUND)
+        if(DOT_EXE_PATH STREQUAL DOT_EXE_PATH-NOTFOUND)
             message(WARNING "Cannot create graphviz visualization of software architecture because executable: ${DOT_EXECUTABLE} not found")
-        else()
-            set(GRAPHVIZ_IMAGE_TARGET graphviz)
-            if(NOT TARGET ${GRAPHVIZ_IMAGE_TARGET})
-                add_custom_target(${GRAPHVIZ_IMAGE_TARGET} ALL
-                    COMMAND ${CMAKE_COMMAND} "--graphviz=graph.dot" .
-                    COMMAND ${DOT_EXECUTABLE} -Tpng graph.dot -o targets.png
-                    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}" # top level of entire build tree
-                )
-            else()
-                message(WARNING "Target: \"${GRAPHVIZ_IMAGE_TARGET}\" already exists.")
-            endif(NOT TARGET ${GRAPHVIZ_IMAGE_TARGET})
-        endif(${DOT_EXE_PATH} STREQUAL DOT_EXE_PATH-NOTFOUND)
+            return()
+        endif(DOT_EXE_PATH STREQUAL DOT_EXE_PATH-NOTFOUND)
+        message(DEBUG "DOT_EXE_PATH:\"${DOT_EXE_PATH}\"")
+
+        set(GRAPHVIZ_IMAGE_TARGET graphviz)
+        if(TARGET ${GRAPHVIZ_IMAGE_TARGET})
+            message(WARNING "Target: \"${GRAPHVIZ_IMAGE_TARGET}\" already exists.")
+            return()
+        endif(TARGET ${GRAPHVIZ_IMAGE_TARGET})
+        
+        add_custom_target(${GRAPHVIZ_IMAGE_TARGET} ALL
+            COMMAND ${CMAKE_COMMAND} "--graphviz=graph.dot" .
+            COMMAND ${DOT_EXECUTABLE} -Tpng graph.dot -o targets.png
+            WORKING_DIRECTORY "${GRAPHVIZ_WORKDIR}"
+        )
+
+        file(WRITE "${GRAPHVIZ_WORKDIR}/graphviz_cleanup.cmake" "cmake_minimum_required(VERSION ${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION})\nfile(GLOB DOT_FILES \"${GRAPHVIZ_WORKDIR}/graph\\.dot\\.*\")\nmessage(DEBUG \"DOT_FILES:\${DOT_FILES}\")\nfile(REMOVE \${DOT_FILES})")
+
+        add_custom_command(
+            TARGET ${GRAPHVIZ_IMAGE_TARGET}
+            POST_BUILD
+            COMMENT "Cleaning up the dot files produced during build of ${GRAPHVIZ_IMAGE_TARGET}"
+            COMMAND ${CMAKE_COMMAND} -P "${GRAPHVIZ_WORKDIR}/graphviz_cleanup.cmake"
+            WORKING_DIRECTORY "${GRAPHVIZ_WORKDIR}"
+        )
     else()
         message(WARNING "Cannot create graphviz visualization of software architecture. Not supported in cmake version : ${CMAKE_VERSION}")
     endif()
